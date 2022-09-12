3ed2747b5afaf1174f4614daf92b59776a652e0f
nfmalano <nfmalano@gmail.com>
2022-09-12 19:14:08 +0700

    Create django app and mapping data to table

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_barang_katalog,
+        'nama': 'Naznien',
+        'npm' : '2106751404'
+    }
+    return render(request, "katalog.html", context)
