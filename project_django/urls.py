03759b8c04bcd7b4a8de8f9774e3c6a4baa2321f
nfmalano <nfmalano@gmail.com>
2022-09-13 10:19:02 +0700

    Change route

--- project_django/urls.py
+++ project_django/urls.py
@@ -18,7 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',  include('katalog.urls'))
+    path('katalog/',  include('katalog.urls'))
 ]
-
-# TODO: Implement Routings Here
