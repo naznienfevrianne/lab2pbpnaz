7174f6db25861d8f0f629d9d71bfb4e278a04d49
nfmalano <nfmalano@gmail.com>
2022-09-28 15:28:12 +0700

    Fixing models

--- todolist/forms.py
+++ todolist/forms.py
@@ -2,10 +2,11 @@ from django.forms import ModelForm
 from django import forms
 from todolist.models import TaskToDoList
 import datetime
+from django.utils.timezone import now
 class TaskForm(ModelForm):
     title  = forms.CharField(max_length=100)
     description = forms.CharField(max_length=1000)
-    date = datetime.datetime.now()
+    date = datetime.datetime.now().date()
 
     class Meta:
         model = TaskToDoList
--- todolist/models.py
+++ todolist/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 import datetime
-from django.utils.timezone import utc
+from django.utils.timezone import now
 # Create your models here.
 
 class TaskToDoList(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user', null=True);
-    date = models.DateTimeField(default=datetime.datetime.now())
+    date = models.DateTimeField(default=datetime.datetime.now().date())
     title = models.CharField(max_length=100)
     description = models.TextField();
 
