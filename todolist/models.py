31edc44a9181b40463d8e6736741df8cd5919601
nfmalano <nfmalano@gmail.com>
2022-09-28 14:54:55 +0700

    Fixing TaskToDoList models

--- todolist/models.py
+++ todolist/models.py
@@ -5,8 +5,8 @@ from django.utils.timezone import utc
 # Create your models here.
 
 class TaskToDoList(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user');
-    date = models.DateTimeField(default=datetime.datetime.now().date())
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user', null=True);
+    date = models.DateTimeField(default=datetime.datetime.now())
     title = models.CharField(max_length=100)
     description = models.TextField();
 
