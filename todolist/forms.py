6b4bc3af9afd6a439b41d7bcd03fbe9d3786e5f3
nfmalano <nfmalano@gmail.com>
2022-09-28 15:32:12 +0700

    Fixing date

--- todolist/forms.py
+++ todolist/forms.py
@@ -6,7 +6,7 @@ from django.utils.timezone import now
 class TaskForm(ModelForm):
     title  = forms.CharField(max_length=100)
     description = forms.CharField(max_length=1000)
-    date = datetime.datetime.now().date()
+    date = datetime.datetime.today().date()
 
     class Meta:
         model = TaskToDoList
--- todolist/models.py
+++ todolist/models.py
@@ -6,7 +6,7 @@ from django.utils.timezone import now
 
 class TaskToDoList(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user', null=True);
-    date = models.DateTimeField(default=datetime.datetime.now().date())
+    date = models.DateTimeField(default=datetime.datetime.today().date())
     title = models.CharField(max_length=100)
     description = models.TextField();
 
